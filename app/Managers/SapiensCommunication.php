57d25b67a8c601a8fdeee781303a2cb72999e2ef
wellington fernandes <wellingtonfds@gmail.com>
2020-04-22 21:38:06 -0300

    update query cc and remove matriz epis

--- app/Managers/SapiensCommunication.php
+++ app/Managers/SapiensCommunication.php
@@ -43,31 +43,44 @@ trait SapiensCommunication
      * Choice Facilities or Gerenciamento
      */
     protected function getCenterOfCost($choice){
+        $today = date('m/d/Y');
         $sql = "
-            SELECT usu_t100ccu.usu_codccu AS 'Usu_CodCcu', 
-       E044CCU.DesCcu AS 'Usu_DesCcu',
-       E085cto.usu_codcar AS 'Usu_CodCar',
-       E085CTO.usu_codent AS 'Usu_CodEnt',
-       E085CTO.NomCto AS 'Usu_NomCto',
-       --R900CPL.EMAIL,
-       MAX(usu_t100con.usu_datbas) AS 'Usu_DatBas'
-       FROM sapiens..usu_t100ccu, sapiens..usu_t100con, sapiens..e044ccu, sapiens..e085cto, sapiens..r900cpl
-  WHERE usu_t100ccu.usu_codemp = 1 
-   AND usu_t100ccu.usu_codemp = usu_t100con.usu_codemp
-   AND usu_t100ccu.usu_codccu = usu_t100con.usu_codccu
-   AND usu_t100ccu.usu_sitctr <> 'I'
-   AND e044ccu.CodEmp = usu_t100ccu.usu_codemp 
-   AND e044ccu.CodCcu = usu_t100ccu.usu_codccu  
-   AND (E044CCU.DesCcu LIKE '%$choice%')
-   AND e085cto.CodCli = usu_t100con.usu_empcon
-   AND e085cto.SeqCto = usu_t100con.usu_codcon
-   AND e085cto.usu_codcar IN (1,2,5,9)
-   GROUP BY usu_t100ccu.usu_codccu, 
-            E085CTO.NomCto,
-            E044CCU.DesCcu,
-            usu_t100con.usu_datbas,
-            E085cto.usu_codcar,
-            E085CTO.usu_codent
+        SELECT usu_t100ccu.usu_codccu AS 'Usu_CodCcu', 
+        E044CCU.DesCcu AS 'Usu_DesCcu',
+        E085cto.usu_codcar AS 'Usu_CodCar',
+        E085CTO.usu_codent AS 'Usu_CodEnt',
+        E085CTO.NomCto AS 'Usu_NomCto',
+        --R900CPL.EMAIL,
+        MAX(usu_t100con.usu_datbas) AS 'Usu_DatBas'
+        FROM sapiens..usu_t100ccu, sapiens..usu_t100con, sapiens..e044ccu, sapiens..e085cto, sapiens..r900cpl
+   WHERE usu_t100ccu.usu_codemp = 1 
+    AND usu_t100ccu.usu_codemp = usu_t100con.usu_codemp
+    AND usu_t100ccu.usu_codccu = usu_t100con.usu_codccu
+    AND usu_t100ccu.usu_sitctr <> 'I'
+    AND e044ccu.CodEmp = usu_t100ccu.usu_codemp 
+    AND e044ccu.CodCcu = usu_t100ccu.usu_codccu  
+    AND (E044CCU.DesCcu LIKE '%$choice%')
+    AND e085cto.CodCli = usu_t100con.usu_empcon
+    AND e085cto.SeqCto = usu_t100con.usu_codcon
+    AND e085cto.usu_codcar IN (1,2,5,9)
+    AND usu_t100con.usu_datbas = (SELECT MAX(A.usu_datbas)       
+                                    FROM sapiens..usu_t100con A,              
+                                         sapiens..e085cto B                   
+                                   WHERE A.usu_codemp = usu_t100con.usu_codemp 
+                                     AND A.usu_codccu = usu_t100con.usu_codccu 
+                                     AND A.usu_empcon = 60                     
+                                     AND B.codcli = 60                         
+                                     AND B.sitcto = 'A'                        
+                                     AND B.codniv = 5                          
+                                     AND A.usu_empcon = B.codcli               
+                                     AND A.usu_codcon = B.seqcto               
+                                     AND A.usu_datbas <= '$today' ) 
+    GROUP BY usu_t100ccu.usu_codccu,
+             E085CTO.NomCto,
+             E044CCU.DesCcu,
+             usu_t100con.usu_datbas,
+             E085cto.usu_codcar,
+             E085CTO.usu_codent
         ";
         try{
             return DB::connection('sqlsrv')->select(DB::raw($sql));
