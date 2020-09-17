a4ede4ac5f797e3953237182a79b2a3f658ddf22
wellington fernandes <wellingtonfds@gmail.com>
2020-09-17 19:38:10 -0300

    fix document

--- app/Http/Controllers/PatternController.php
+++ app/Http/Controllers/PatternController.php
@@ -13,54 +13,57 @@ use Illuminate\Support\Facades\Storage;
 class PatternController extends Controller
 {
     use SapiensCommunication;
-    public function index(){
-        if(Auth::check())
+    public function index()
+    {
+        if (Auth::check())
             return view('pattern.index');
-        $birthDays =[];
-        try{
+        $birthDays = [];
+        try {
             $birthDays = $this->getBirthdays();
-        }catch (\Exception $e){
+        } catch (\Exception $e) {
             $birthDays = 'null';
         }
-        $posts = Post::where('status_post_id','=',2)->orderBy('created_at','DESC')->paginate(5);
-        return view('pattern.index',['posts'=>$posts,'birthDays'=>$birthDays]);
+        $posts = Post::where('status_post_id', '=', 2)->orderBy('created_at', 'DESC')->paginate(5);
+        return view('pattern.index', ['posts' => $posts, 'birthDays' => $birthDays]);
     }
 
-    public function store(Document $document,Request $request){
-        $this->validate($request,[
-            'file'=>'required',
-            'categorization_id'=>'required',
-            'discipline_id'=>'required',
-            'sub_discipline_id'=>'required',
-            'title'=>'required',
-            'type'=>'required'
+    public function store(Document $document, Request $request)
+    {
+        $this->validate($request, [
+            'file' => 'required',
+            'categorization_id' => 'required',
+            'discipline_id' => 'required',
+            'sub_discipline_id' => 'required',
+            'title' => 'required',
+            'type' => 'required'
         ]);
         $document->fill($request->all());
         $document->file = $request->file('file')[0]->store('public/document');
         $document->save();
         return $document;
     }
-    public function update(Request $request,Document $document){
+    public function update(Request $request, Document $document)
+    {
         $document->fill($request->all());
-        if($request->hasFile('file')){
+        if ($request->hasFile('file')) {
             $document->file = $request->file('file')[0]->store('public/document');
         }
         $document->save();
         return $document;
     }
 
-    public function listDocuments(){
+    public function listDocuments()
+    {
         $documents =  Document::with('Discipline')->with('SubDiscipline')->with('Category')->get();
         return $documents;
     }
-    public function destroy(Document $document){
+    public function destroy(Document $document)
+    {
         $document->delete();
         return $document;
     }
-    public function show(Document $document){
-        //dd($document);
-        $path = str_replace('/','\\',$document->file);
-        return response()->download(storage_path('app\\'.$path));
-        
+    public function show(Document $document)
+    {
+        return response()->download(storage_path("app/" . $document->file));
     }
 }
